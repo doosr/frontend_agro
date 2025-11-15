aebf96349fa1d290c124398c7317587efc61ebeb
Dawser_belgacem <dawser.belgacem@mahdia.r-iset.tn>
2025-11-15 12:04:00 +0100

    efe

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 // src/config/api.js
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://backendagro.vercel.app/api';
 
 const api = axios.create({
   baseURL: API_URL,
