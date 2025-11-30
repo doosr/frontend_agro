e04dad202d9c744aa35ab6afe6005ac45e4877aa
Dawser_belgacem <dawser.belgacem@mahdia.r-iset.tn>
2025-11-30 13:43:16 +0100

    fix: Add forgotPassword and resetPassword to authService

--- src/services/authService.js
+++ src/services/authService.js
@@ -37,6 +37,19 @@ const authService = {
 
   updateUser: (user) => {
     localStorage.setItem('user', JSON.stringify(user));
+  },
+
+  forgotPassword: async (email) => {
+    const response = await api.post('/auth/forgot-password', { email });
+    return response.data;
+  },
+
+  resetPassword: async (token, newPassword) => {
+    const response = await api.post('/auth/reset-password', {
+      token,
+      newPassword
+    });
+    return response.data;
   }
 };
 
